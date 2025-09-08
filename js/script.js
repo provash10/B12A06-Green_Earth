46597a1bd808b3dd8e96f6447b4709880743e95e
provash10 <provash20cb@gmail.com>
2025-09-08 22:17:45 +0600

    Edit function

--- js/script.js
+++ js/script.js
@@ -24,25 +24,25 @@ const displayCategories = (categories) => {
         btnDiv.innerHTML = `
                    <button class="font-semibold px-2 m-2 w-fit rounded-lg hover:bg-[#16a34a]">${category.category_name}</button>
 
+            </button>
         `;
         categoriesContainer.append(btnDiv);
     }
 
 };
 
-// load categories all products 2Q
-const loadProducts = () => {
+// load categories all Card 2Q
+const loadCard = () => {
     fetch("https://openapi.programming-hero.com/api/plants")
         .then((res) => res.json())
-        .then(json => displayProducts(json.plants))
+        .then(json => displayCard(json.plants))
 };
 
-const displayProducts = (products) => {
-    console.log(products); // checked
-    const productsContainer = document.getElementById("products-container");
-    productsContainer.innerHTML = "";
-
-    for (let product of products) {
+const displayCard = (cards) => {
+    // console.log(Card); // checked
+    const CardContainer = document.getElementById("card-container");
+    CardContainer.innerHTML = "";
+    for (let card of cards) {
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
 
@@ -66,10 +66,17 @@ const displayProducts = (products) => {
 
         `;
 
-        productsContainer.append(btnDiv);
+        CardContainer.append(btnDiv);
     }
 
 };
 
-loadProducts();
+//load Card by category
+
+
+
+
+
+
+loadCard();
 loadCategories();
